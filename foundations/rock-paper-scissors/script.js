005373d2797e4fc903c1eb866cdf36e09e896874
wolfie-ssh <tilakg0p1605@gmail.com>
2025-11-13 12:48:17 +0530

    add playRound fn logic added logic for function playRound() which takes in input and shows the winner along with the scores

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -19,7 +19,54 @@ function getHumanChoice() {
     return HumanChoice;
 }
 
+function playRound(computerChoice, humanChoice) 
+{
+    function humanWin() {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }
+
+    function humanLose() {
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+    }
+
+
+if(computerChoice == "rock" && humanChoice == "paper") {
+    humanWin();
+    humanScore++;
+}
+
+else if(computerChoice == "paper" && humanChoice == "scissors" ) {
+    humanWin();
+    humanScore++;
+}
+
+else if(computerChoice == "scissors" && humanChoice == "rock") {
+    humanWin();
+    humanScore++;
+}
+
+else if(computerChoice == "rock" && humanChoice == "scissors") {
+    humanLose();
+    computerScore++;
+}
+
+else if(computerChoice == "paper" && humanChoice == "rock") {
+    humanLose();
+    computerScore++;
+}
+
+else if(computerChoice == "scissors" && humanChoice == "paper") {
+    humanLose();
+    computerScore++;
+}
+else {
+    console.log("Draw");
+}
+console.log(`Your Score is ${humanScore} vs ComputerScore ${computerScore}`);
+}
+
 let computerChoice = getComputerChoice();
 let humanChoice = getHumanChoice();
+playRound(computerChoice, humanChoice);
 let humanScore = 0;
 let computerScore = 0;
